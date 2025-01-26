eb0dfc0a61c74235a4e2e583c50a8289921c1d65
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-01-26 22:41:35 +0530

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,23 @@ function Footer(){
     return(
         <>
             <div className='footerContainer'>
+                <div className='upperFooter'>
+                    <div className='link'>
+                        <a href='https://www.npmjs.com/package/xceptions'>Npm Repository</a>
+                        <a href='https://github.com/miteshp2110/Xceptions.git'>Github Repository</a>
+                        <a href='https://github.com/miteshp2110/Xception-backend.git'>Service Repository</a>
+                    </div>
+                    <div className='connectionLink'>
+                        <a href='https://linkedin.com/in/miteshp2110
+
+'>Linkdin</a>
+                        <a href='https://mail.google.com/mail/?view=cm&fs=1&to=paliwalmitesh2110@gmail.com'>gmail</a>
+                        <a href='https://github.com/miteshp2110'>Github</a>
+                    </div>
+                </div>
+                <div className='baseFooter'>
+                    Developed by Mitesh Paliwal
+                </div>
 
             </div>
         </>
